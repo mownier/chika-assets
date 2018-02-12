7855a2da6b605233f684eacc87d1522ec239a3a4
Mounir Ybanez <rinuom91@gmail.com>
2018-02-12 13:13:13 +0800

    Initial commit.

---
+++ ChikaAssets/ChikaAssets.h
@@ -0,0 +1,19 @@
+//
+//  ChikaAssets.h
+//  ChikaAssets
+//
+//  Created by Mounir Ybanez on 2/8/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChikaAssets.
+FOUNDATION_EXPORT double ChikaAssetsVersionNumber;
+
+//! Project version string for ChikaAssets.
+FOUNDATION_EXPORT const unsigned char ChikaAssetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChikaAssets/PublicHeader.h>
+
+
